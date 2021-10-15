75feda6c76e40e9707eb02257564521f5ff63a05
tyikyi <ddale910@gmail.com>
2021-10-15 14:59:26 +0630

    First commit

--- tutorial1.php
+++ tutorial1.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Tutorial1</title>
+    <title>Tutorial1</title>gi
 </head>
 <body>
 <table style="margin: auto">
